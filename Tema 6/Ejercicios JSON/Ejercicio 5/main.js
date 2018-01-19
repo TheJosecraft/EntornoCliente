d86158e983367c090ec34ef657b605c944884ec1
rokkay <josekawa1996@gmail.com>
2018-01-19 11:37:46 +0100

    Arreglado Ejercicio 5

--- Tema 6/Ejercicios JSON/Ejercicio 5/main.js
+++ Tema 6/Ejercicios JSON/Ejercicio 5/main.js
@@ -1,84 +1,89 @@
 $(document).ready(function () {
 
-    var clientes = {
-        "cliente1": {
+    var clientes = [
+        {
             "nombre": "José Carlos",
             "dni": "31021855F",
-            "productos": {
-                "producto1": {
+            "productos": [
+                {
                     "referencia": "#00221",
                     "nombre": "Procesador i5-3470",
-                    "precio": "112"
+                    "precio": 112
                 },
-                "producto2": {
+                {
                     "referencia": "#00237",
                     "nombre": "Gráfica RX 480 8GB",
-                    "precio": "220"
+                    "precio": 220.5
                 }
-            }
+            ]
         },
-        "cliente2": {
+        {
             "nombre": "Juan Carlos",
             "dni": "84357849C",
-            "productos": {
-                "producto1": {
+            "productos": [
+                {
                     "referencia": "#00321",
                     "nombre": "NVIDIA TITAN XP",
-                    "precio": "1250"
+                    "precio": 1199.90
                 },
-                "producto2": {
+                {
                     "referencia": "#03687",
                     "nombre": "Xiaomi Redmi Note 4 SD",
-                    "precio": "114"
+                    "precio": 114
                 }
-            }
+            ]
         },
-        "cliente3": {
+        {
             "nombre": "José Antonio",
             "dni": "31542367P",
-            "productos": {
-                "producto1": {
+            "productos": [
+                {
                     "referencia": "#02341",
                     "nombre": "SSD Crucial MX 300",
-                    "precio": "95"
+                    "precio": 95
                 },
-                "producto2": {
+                {
                     "referencia": "#32423",
                     "nombre": "Pantall Benq 27",
-                    "precio": "220"
+                    "precio": 220
                 }
+            ]
+        }
+    ];
+
+    var info = $("#info");
+    console.log(clientes);
+
+    $("#mostrar").click(function () {
+
+        var dni = prompt("Introduce el dni del cliente");
+        var datos;
+
+        for (var cliente in clientes) {
+            if (dni == clientes[cliente]["dni"]) {
+                datos = clientes[cliente];
             }
         }
-    }
 
-    var dni = prompt("Introduce el dni del cliente");
-    var datos;
+        var productos = datos["productos"];
 
-    for (var cliente in clientes) {
-        if (dni == clientes[cliente]["dni"]) {
-            datos = clientes[cliente];
+        var dinero = 0;
+
+        info.append("<h3>Nombre</h3>");
+        info.append("<p>" + datos["nombre"] + "</p>");
+
+        info.append("<h3>Productos</h3>");
+        for (var producto in productos) {
+            info.append("<p>" + productos[producto]["nombre"] + "</p>");
+            dinero += productos[producto]["precio"];
         }
-    }
-    
-    var info = $("#info");
-    
-    var productos = datos["productos"];
-    
-    var dinero = 0;
-    
-    info.append("<h3>Nombre</h3>");
-    info.append("<p>" + datos["nombre"] + "</p>");
-    
-    info.append("<h3>Productos</h3>");
-    for(var producto in productos){
-        info.append("<p>" + productos[producto]["nombre"] + "</p>");
-        dinero += parseInt(productos[producto]["precio"]);
-    }
-    
-    info.append("<h3>Precio</h3>");
-    
-    info.append("<p>" + dinero + " €</p>");
-    
-    console.log(datos);
+
+        info.append("<h3>Precio</h3>");
+
+        info.append("<p>" + dinero + " €</p>");
+
+        console.log(datos);
+
+    });
 
 });
