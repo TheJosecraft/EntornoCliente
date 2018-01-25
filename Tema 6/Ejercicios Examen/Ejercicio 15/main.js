f974bd3a074902d9a8ecca85514125268e0c2896
rokkay <josekawa1996@gmail.com>
2018-01-25 10:57:24 +0100

    Ejercicio 15

---
+++ Tema 6/Ejercicios Examen/Ejercicio 15/main.js
@@ -0,0 +1,31 @@
+//EJERCICIO 15
+//Partiendo del ejercicio 12 hacer que cada una de las celdas de la tabla tenga un
+//color RGB aleatorio en el momento de crearse.
+
+$(document).ready(function () {
+
+
+    $("body").append("<table id=tabla>");
+    for (var i = 0; i < 20; i++) {
+        $("#tabla").append("<tr id=" + i + ">");
+
+        for (var j = 0; j < 20; j++) {
+            $("tr#" + i).append("<td></td>");
+        }
+
+        $("#tabla").append("</tr>");
+    }
+    $("body").append("</table>");
+
+    var celdas = $("#tabla tr td");
+    
+    for(var i = 0; i <= celdas.length; i++){
+        var red = Math.floor(Math.random() * (255 - 0));
+        var green = Math.floor(Math.random() * (255 - 0));
+        var blue = Math.floor(Math.random() * (255 - 0));
+        console.log("rgb(" + red + ", " + green + ", " + blue + ")")
+        
+        celdas[i].css("background-color", "rgb(" + red + ", " + green + ", " + blue + ")");
+    }
+
+});
