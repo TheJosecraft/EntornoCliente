0456262f9f437e8a3c0864d538b8c0c969bf73df
José Carlos <josekawa1996@gmail.com>
2018-02-13 14:33:19 +0100

    AJAX

---
+++ Tema 6/AJAX/horoscopo.php
@@ -0,0 +1,49 @@
+<!-- Aries -->
+<?php if($_GET["cod"] == 1) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Tauro -->
+<?php if($_GET["cod"] == 2) : ?>
+<p>La estabilidad la permanencia. La fuerza de voluntad, la permanencia, la firmeza, la perseverancia, la estabilidad, la concreción, la resistencia, la fuerza, la sensualidad la buena mesa, la forma, la música, el talento vocal, los sentimientos duraderos, la naturaleza, la agricultura y la banca.</p>
+<?php endif; ?>
+<!-- Géminis -->
+<?php if($_GET["cod"] == 3) : ?>
+<p>La dualidad de la mente, la capacidad de ver las dos caras de una misma moneda y la versatilidad. El elemento aéreo, las comunicaciones, la dualidad, la iniciativa en los negocios, el comercio, el trabajo intelectual, los viajes, los hermanos, los amigos de la infancia, los escritos, los idiomas y la mente concreta.</p>
+<?php endif; ?>
+<!-- Cáncer -->
+<?php if($_GET["cod"] == 4) : ?>
+<p>Representa las altas y bajas emocionales que se mueven como el cangrejo y que finalmente se esconden en su caparazón. La maternidad, los partos, el amor maternal, la familia, la patria la casa, la cocina, las masas, las mujeres, los ríos, el sentimiento y la imaginación.
+</p>
+<?php endif; ?>
+<!-- Leo -->
+<?php if($_GET["cod"] == 5) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Virgo -->
+<?php if($_GET["cod"] == 6) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Libra -->
+<?php if($_GET["cod"] == 7) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Escorpio -->
+<?php if($_GET["cod"] == 8) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Sagitario -->
+<?php if($_GET["cod"] == 9) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Capricornio -->
+<?php if($_GET["cod"] == 10) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Acuario -->
+<?php if($_GET["cod"] == 11) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
+<!-- Piscis -->
+<?php if($_GET["cod"] == 12) : ?>
+<p>Un Aries es una persona llena de energía y entusiasmo. Pionero y aventurero, le encantan los retos, la libertad y las nuevas ideas. A los Aries les gusta liderar y prefieren dar instrucciones a recibirlas. Son independientes y preocupados por su propia ambición y objetivos. Tienen una energía envidiable que a veces les lleva a ser agresivos, inquietos, argumentativos, tercos. Es fácil ofender a los Aries y, cuando se sienten ofendidos, es difícil hacer las paces con ellos. Aries es el primer signo del zodiaco y, en este sentido, su papel es empezar algo y liderar. Si un Aries empieza a creer en una buena causa, luchará sin descanso para promocionarla.</p>
+<?php endif; ?>
---
+++ Tema 6/AJAX/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	$(document).ajaxStart(function(){
+
+	}).ajaxStop(function(){
+		
+	})
+
+	$("p a").click(function(event){
+		event.preventDefault();
+		var url = $(this).attr("href");
+		console.log(url);
+		$.get(url, AjaxGet);
+	})
+
+});
+
+function AjaxGet(datos){
+	$("#info").html(datos);
+}
